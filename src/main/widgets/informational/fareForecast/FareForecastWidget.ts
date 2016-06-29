63c67f3e0418eb49d8e591ed61d1b48ca2011064
Szymanski, Tomasz <Tomasz.Szymanski@sabre.com>
2016-06-29 16:28:08 +0200

    Fare Forecast widget: refactor to use base controller, to reuse base controller functionality.

--- src/main/widgets/informational/fareForecast/FareForecastWidget.ts
+++ src/main/widgets/informational/fareForecast/FareForecastWidget.ts
@@ -3,60 +3,74 @@ define([
         , 'widgets/SDSWidgets'
         , 'webservices/informational/FareForecastDataService'
         , 'widgets/WidgetGlobalCallbacks'
+        , 'widgets/BaseController'
     ],
     function (
           angular
         , SDSWidgets
         , FareForecastDataServiceSrc
         , WidgetGlobalCallbacks
+        , BaseController
     ) {
         'use strict';
 
+        function FareForecastController($scope, SearchCriteriaBroadcastingService, newSearchCriteriaEvent, FareForecastDataService) {
+            BaseController.call(this, {
+                scope: $scope
+                , searchService: {
+                    executeSearch: FareForecastDataService.getFareForecast
+                }
+                , newSearchCriteriaEvent: newSearchCriteriaEvent
+                , searchCriteriaBroadcastingService: SearchCriteriaBroadcastingService
+            });
+
+            function initializeEmptyModel() {
+                $scope.fareForecast = {};
+            }
+
+            initializeEmptyModel();
+
+            this.processSearchResults = function (fareForecast) {
+                $scope.fareForecast.recommendation = fareForecast.recommendation;
+            };
+
+            this.clearModel = function () {
+                initializeEmptyModel();
+            };
+
+            this.isAnyDataToDisplayAvailable = function () {
+                return ($scope.fareForecast.recommendation);
+            };
+
+            /**
+             * Method enforcing the business rules, defined as data attributes to the directive, whether to show wait and unknown recommendations.
+             * By default wait and unknown recommendations are not shown, unless requested by directive attributes.
+             * @returns {boolean}
+             */
+            this.showRecommendationPolicy = function () {
+                if ($scope.fareForecast.recommendation === 'wait') {
+                    return $scope.showWaitRecommendation;
+                }
+                if ($scope.fareForecast.recommendation === 'unknown') {
+                    return $scope.showUnknownRecommendation;
+                }
+                // for buy recommendation always show
+                return true;
+            };
+
+            return this;
+        }
+
+        FareForecastController.prototype = Object.create(BaseController.prototype);
+        FareForecastController.prototype.constructor = FareForecastController;
+
         return angular.module('sdsWidgets')
-            .controller('FareForecastCtrl', [
+            .controller('FareForecastController', [
                   '$scope'
                 , 'SearchCriteriaBroadcastingService'
                 , 'newSearchCriteriaEvent'
                 , 'FareForecastDataService'
-                , function (
-                      $scope
-                    , SearchCriteriaBroadcastingService
-                    , newSearchCriteriaEvent
-                    , FareForecastDataService
-                ) {
-
-                    // only model object
-                    $scope.fareForecast = {};
-
-                    // main controller function, acting on new search criteria sent to the widget
-                    $scope.$on(newSearchCriteriaEvent, function () {
-                        var newSearchCriteria = SearchCriteriaBroadcastingService.searchCriteria;
-                        FareForecastDataService.getFareForecast(newSearchCriteria).then(function (fareForecast) {
-                            $scope.fareForecast.recommendation = fareForecast.recommendation;
-                        });
-                    });
-
-                    $scope.isAnyDataToDisplayAvailable = function () {
-                        return ($scope.fareForecast.recommendation);
-                    };
-
-                    /**
-                     * Method enforcing the business rules, defined as data attributes to the directive, whether to show wait and unknown recommendations.
-                     * By default wait and unknown recommendations are not shown, unless requested by directive attributes.
-                     * @returns {boolean}
-                     */
-                    $scope.showRecommendation = function () {
-                        if ($scope.fareForecast.recommendation === 'wait') {
-                            return $scope.showWaitRecommendation;
-                        }
-                        if ($scope.fareForecast.recommendation === 'unknown') {
-                            return $scope.showUnknownRecommendation;
-                        }
-                        // for buy recommendation always show
-                        return true;
-                    };
-
-                }])
+                , FareForecastController])
             .directive('fareForecast', function () {
                 return {
                     restrict: 'AE',
@@ -65,8 +79,10 @@ define([
                         , showUnknownRecommendation: '@'
                     },
                     templateUrl: '../widgets/view-templates/widgets/FareForecastWidget.tpl.html',
-                    controller: 'FareForecastCtrl',
-                    link: function () {
+                    controller: 'FareForecastController',
+                    controllerAs: 'ctrl',
+                    link: function (scope, element, attrs, controller) {
+                        controller.executeLifeSearchOnPredefinedCriteriaIfPresent(attrs.origin, attrs.destination, attrs.departureDate, attrs.returnDate);
                         WidgetGlobalCallbacks.linkComplete();
                     }
                 };
