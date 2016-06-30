34e9d3e1a337d8cb71c3a501166e17c9b2bb3450
Szymanski, Tomasz <Tomasz.Szymanski@sabre.com>
2016-06-30 16:01:46 +0200

    Destination Finder: parse date strings from webservice into moment dates (to be used by clients, views in particular, for displaying dates)

--- src/main/webservices/inspirational/DestinationFinderDataService.ts
+++ src/main/webservices/inspirational/DestinationFinderDataService.ts
@@ -55,8 +55,11 @@ define([
                             return $q(function(resolve, reject) {
                                 var requestParams = translateSearchCriteriaIntoRequestParams(searchCriteria);
                                 DestinationFinderWebService.get(requestParams).$promise.then(
-                                    function (response) {
-                                        resolve(response);
+                                    function (pricesForDestinations) {
+                                        pricesForDestinations.FareInfo = pricesForDestinations.FareInfo
+                                            .filter((offer) => isFinite(offer.LowestFare.Fare)) //WARN: LowestFare from API may be also just string "N/A"
+                                            .map(parseTravelDates);
+                                        resolve(pricesForDestinations);
                                     },
                                     function (reason) {
                                         ErrorReportingService.reportError('Could not get prices for destinations', searchCriteria);
@@ -64,6 +67,16 @@ define([
                                     }
                                 );
                             });
+
+                            function parseTravelDates(offer) {
+                                const copyWithDatesParsed = _.extend({}, offer, {
+                                    departureDateTime: moment(offer.DepartureDateTime, moment.ISO_8601),
+                                    returnDateTime: moment(offer.ReturnDateTime, moment.ISO_8601)
+                                });
+                                delete copyWithDatesParsed.DepartureDateTime;
+                                delete copyWithDatesParsed.ReturnDateTime;
+                                return copyWithDatesParsed;
+                            }
                         }
                     };
                 }]);
