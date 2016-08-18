ade5e9f561ee12085f752bdc4b1dcee0e9074f13
Szymanski, Tomasz <Tomasz.Szymanski@sabre.com>
2016-08-18 08:29:38 +0200

    Move filters panel unit tests to separate module

--- src/test/util/LodashExtensionsTest.ts
+++ src/test/util/LodashExtensionsTest.ts
@@ -6,25 +6,6 @@ define([
     , __
 ) {
     "use strict";
-    describe('groupByAndGetCountAndMin', function () {
-        it('groupByAndGetCountAndMin', function () {
-            var employees = [
-                  {name: 'fred', department: 'sales', salary: 200}
-                , {name: 'john', department: 'sales', salary: 150}
-                , {name: 'anna', department: 'finance', salary: 300}
-            ];
-            // get number of employees per department and minimum salary per department
-            var allGroupings = __.groupByAndGetCountAndMin(employees, 'department', 'salary');
-
-            var groupingItemForSalesDep = _.find(allGroupings, 'value', 'sales');
-            expect(groupingItemForSalesDep.count).toBe(2);
-            expect(groupingItemForSalesDep.min).toBe(150);
-
-            var groupingItemForFinanceDep = _.find(allGroupings, 'value', 'finance');
-            expect(groupingItemForFinanceDep.count).toBe(1);
-            expect(groupingItemForFinanceDep.min).toBe(300);
-        });
-    });
 
     describe('leafValues', function () {
         it('one level map', function () {
