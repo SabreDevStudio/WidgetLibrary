15c4251766f71cef7a27b622f7923815c1568b2b
Szymanski, Tomasz <Tomasz.Szymanski@sabre.com>
2016-08-26 15:57:24 +0200

    refactor configuration

--- src/main/Configuration.ts
+++ src/main/Configuration.ts
@@ -6,22 +6,34 @@ define([
     ) {
         'use strict';
 
+        const configs = {
+            US: {
+                apiURL: 'http://airports.ds36.net/api/sabre', //'http://localhost:8088/sabreapibridge/api'
+                apiSpecificHeaders: {
+                    'application-key': 'YWQ3NmM5ZWYtZGNmYi00ODA0LTg0YzgtNjYwNGYzOGJkMzhhNGE4YzY5NWQtMjZjMy00NmZmLTg5ZDEtYzVhODU0MzYwN2Uz'
+                },
+                pointOfSaleCountry: 'US',
+                bfmRequestPcc: 'E8KE'
+            },
+            DE: {
+                apiURL: 'http://10.14.54.160:8088/sabreapibridge/api',
+                apiSpecificHeaders: {},
+                pointOfSaleCountry: 'DE',
+                bfmRequestPcc: 'E8KE'
+            }
+        };
+
+        const selectedConfig = configs.DE;
+
         var configurationModule = angular.module('configuration', [])
-            //.value('apiURL', 'http://localhost:8088/sabreapibridge/api')
-            .value('apiURL', 'http://10.14.54.160:8088/sabreapibridge/api')
-            //.value('apiURL', 'http://airports.ds36.net/api/sabre')
-            //.value('apiSpecificHeaders', {
-            //    'application-key': 'YWQ3NmM5ZWYtZGNmYi00ODA0LTg0YzgtNjYwNGYzOGJkMzhhNGE4YzY5NWQtMjZjMy00NmZmLTg5ZDEtYzVhODU0MzYwN2Uz'
-            //})
-            .value('apiSpecificHeaders', {})
-            .value('pointOfSaleCountry', 'DE')
-            //.value('pointOfSaleCountry', 'US')
+            .value('apiURL', selectedConfig.apiURL)
+            .value('apiSpecificHeaders', selectedConfig.apiSpecificHeaders)
+            .value('pointOfSaleCountry', selectedConfig.pointOfSaleCountry)
+            .value('bfmRequestPcc', selectedConfig.bfmRequestPcc)
+
             .value('fareNabberApiURL', 'http://pifhli101:51000/v1/AirShopping/FNB_Subscriptions') // address of resource to create Fare Nabber subscriptions DAILY
             //.value('fareNabberApiURL', 'http://ttfhli502:51000/v1/AirShopping/FNB_Subscriptions') // address of resource to create Fare Nabber subscriptions INT
             .value('fareNabberRegistrationPCC', 'E8KE')
-
-            //.value('bfmRequestPcc', 'E8KE')
-            .value('bfmRequestPcc', 'E8KE')
 
         return configurationModule;
     });
